fc7d1a8658beb45e0684153f46c83562e6b613df
an <pistazie@gmx.com>
2021-04-25 19:34:40 +0300

    hw04-gc gc benchmark

---
+++ hw04-gc/src/main/java/ru/java/pro/BenchmarkMBean.java
@@ -0,0 +1,7 @@
+package ru.java.pro;
+
+public interface BenchmarkMBean {
+  int getSize();
+
+  void setSize(int size);
+}
---
+++ hw04-gc/src/main/java/ru/java/pro/GcTest.java
@@ -0,0 +1,73 @@
+package ru.java.pro;
+
+import com.sun.management.GarbageCollectionNotificationInfo;
+
+import javax.management.MBeanServer;
+import javax.management.NotificationEmitter;
+import javax.management.NotificationListener;
+import javax.management.ObjectName;
+import javax.management.openmbean.CompositeData;
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.List;
+
+/*
+О формате логов
+http://openjdk.java.net/jeps/158
+
+
+-Xms512m
+-Xmx512m
+-Xlog:gc=debug:file=./logs/gc-%p-%t.log:tags,uptime,time,level:filecount=5,filesize=10m
+-XX:+HeapDumpOnOutOfMemoryError
+-XX:HeapDumpPath=./logs/dump
+-XX:+UseG1GC
+
+-XX:+UseZGC
+-XX:+UseSerialGC
+-XX:+UseParallelGC
+-XX:+UseShenandoahGC
+
+mac os use full path, create folder
+*/
+public class GcTest {
+
+    public static void main(String... args) throws Exception {
+        System.out.println("Starting pid: " + ManagementFactory.getRuntimeMXBean().getName());
+        switchOnMonitoring();
+        long beginTime = System.currentTimeMillis();
+
+        int size = 500;
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("ru.java.pro:type=Benchmark");
+
+        Benchmark mbean = new Benchmark();
+        mbs.registerMBean(mbean, name);
+        mbean.setSize(size);
+        mbean.run();
+
+        System.out.println("time:" + (System.currentTimeMillis() - beginTime) / 1000);
+    }
+
+    private static void switchOnMonitoring() {
+        List<GarbageCollectorMXBean> gcBeans = ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean gcBean : gcBeans) {
+            System.out.println("GC name:" + gcBean.getName());
+            NotificationEmitter emitter = (NotificationEmitter) gcBean;
+            NotificationListener listener = (notification, handback) -> {
+                if (notification.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
+                    GarbageCollectionNotificationInfo info = GarbageCollectionNotificationInfo.from((CompositeData) notification.getUserData());
+                    String gcName = info.getGcName();
+                    String gcAction = info.getGcAction();
+                    String gcCause = info.getGcCause();
+
+                    long startTime = info.getGcInfo().getStartTime();
+                    long duration = info.getGcInfo().getDuration();
+
+                    System.out.println("start:" + startTime + " Name:" + gcName + ", action:" + gcAction + ", gcCause:" + gcCause + "(" + duration + " ms)");
+                }
+            };
+            emitter.addNotificationListener(listener, null, null);
+        }
+    }
+}
