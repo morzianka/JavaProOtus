0e185dfc04fa042d5c971e2f0c02ffebd9a547e3
an <pistazie@gmx.com>
2021-10-24 10:39:18 +0300

    hw17-multiprocessing hw

---
+++ hw17-multiprocessing/src/main/java/ru/otus/Application.java
@@ -0,0 +1,15 @@
+package ru.otus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Shabunina Anita
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
